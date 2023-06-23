2a7ae13746518d15da7c7ce2fd3f679a4e7ee009
vanwolf16 <d.murillo28@yahoo.com>
2023-06-23 14:24:39 -0700

    Added a NavBar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 // page components
+import Navbar from './components/Navbar';
 import Home from './pages/home/Home';
 import Create from './pages/create/Create';
 import Search from './pages/search/Search';
@@ -11,14 +12,16 @@ import { Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-
+      
+      <Navbar />
+      
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/create' element={<Create />}/>
         <Route path='/search' element={<Search />}/>
-        <Route path='/recipes' element={<Recipe />}/>
+        <Route path='/recipes/:id' element={<Recipe />}/>
       </Routes>
-      
+
     </div>
   );
 }
