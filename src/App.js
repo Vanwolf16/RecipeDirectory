8ace5d25412f513eea0db524009ffe63267ccadb
vanwolf16 <d.murillo28@yahoo.com>
2023-06-23 12:43:19 -0700

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+// page components
+import Home from './pages/home/Home';
+import Create from './pages/create/Create';
+import Search from './pages/search/Search';
+import Recipe from './pages/recipe/Recipe';
+import { Route, Routes } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/create' element={<Create />}/>
+        <Route path='/search' element={<Search />}/>
+        <Route path='/recipes' element={<Recipe />}/>
+      </Routes>
+      
     </div>
   );
 }
